06241409f07ed03350aed6261345d4b81f35328b
zhangdi <892406311@qq.com>
2024-01-23 00:44:36 +0800

    dev_0122

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseMapperTests.java
@@ -0,0 +1,51 @@
+package com.xuecheng.content;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class CourseBaseMapperTests {
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+    @Test
+    public void testCourseBaseMapper(){
+        CourseBase courseBase = courseBaseMapper.selectById(18);
+        Assertions.assertNotNull(courseBase);
+        //查询条件
+        QueryCourseParamsDto courseParamsDto = new QueryCourseParamsDto();
+        courseParamsDto.setCourseName("java");
+        //拼装查询条件
+        LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
+        //根据名称模糊查询,在sql中拼接course_base.name like '%值%'
+        queryWrapper.like(StringUtils.isNotBlank(courseParamsDto.getCourseName()),CourseBase::getName,courseParamsDto.getCourseName());
+        //根据课程审核状态查询 course_base.audit_status = ?
+        queryWrapper.eq(StringUtils.isNotBlank(courseParamsDto.getAuditStatus()),CourseBase::getAuditStatus,courseParamsDto.getAuditStatus());
+        //todo:按课程发布状态查询
+        PageParams pageParams = new PageParams();
+        pageParams.setPageNo(1L);
+        pageParams.setPageSize(2L);
+        //创建分页参数对象，当前页码，每页记录数
+        Page<CourseBase> page = new Page<>(1,2);
+        Page<CourseBase> pageResult = courseBaseMapper.selectPage(page, queryWrapper);
+        //数据列表
+        List<CourseBase> items = pageResult.getRecords();
+        //总记录数
+        long total = pageResult.getTotal();
+
+        //List<T> items, long courts, long page, long pageSize
+        PageResult<CourseBase> courseBasePageResult = new PageResult<CourseBase>(items,total, pageParams.getPageNo(), pageParams.getPageSize());
+        System.out.println(courseBasePageResult);
+    }
+}
