3bd96f0297fe49bc4ea14cfa43bd17ac11b2c077
zhangdi <892406311@qq.com>
2024-01-23 21:19:40 +0800

    dev0123_1

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/Service/CourseBaseInfoService.java
@@ -0,0 +1,13 @@
+package com.xuecheng.content.Service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import org.springframework.web.bind.annotation.RequestBody;
+
+//课程信息管理接口
+public interface CourseBaseInfoService {
+    //课程分页查询
+    public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseInfoServiceTests.java
@@ -0,0 +1,38 @@
+package com.xuecheng.content;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.Service.CourseBaseInfoService;
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class CourseBaseInfoServiceTests {
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+    @Test
+    public void testCourseBaseMapper(){
+        //查询条件
+        QueryCourseParamsDto courseParamsDto = new QueryCourseParamsDto();
+        courseParamsDto.setCourseName("java");
+        courseParamsDto.setAuditStatus("202004");
+
+        PageParams pageParams = new PageParams();
+        pageParams.setPageNo(1L);
+        pageParams.setPageSize(2L);
+
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, courseParamsDto);
+        System.out.println(courseBasePageResult);
+
+    }
+}
