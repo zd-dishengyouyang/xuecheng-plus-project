e4f6a086c9aaed90b612af95a5a1650a80b775b6
zhangdi <892406311@qq.com>
2024-02-25 18:47:39 +0800

    课程发布分布式事务处理

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CoursePublishController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CoursePublishController.java
@@ -2,6 +2,8 @@ package com.xuecheng.content.api;
 
 import com.xuecheng.content.Service.CoursePublishService;
 import com.xuecheng.content.model.dto.CoursePreviewDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
  * @date 2022/9/16 14:48
  * @version 1.0
  */
+@Api(value = "课程预览发布接口",tags = "课程预览发布接口")
 @Controller
 public class CoursePublishController {
 
@@ -23,11 +26,11 @@ public class CoursePublishController {
     CoursePublishService coursePublishService;
 
     @GetMapping("/coursepreview/{courseId}")
-    public ModelAndView preview(@PathVariable("courseId") Long courseId){
+    public ModelAndView preview(@PathVariable("courseId") Long courseId) {
 
         ModelAndView modelAndView = new ModelAndView();
         CoursePreviewDto coursePreviewInfo = coursePublishService.getCoursePreviewInfo(courseId);
-        modelAndView.addObject("model",coursePreviewInfo);
+        modelAndView.addObject("model", coursePreviewInfo);
         modelAndView.setViewName("course_template");
         return modelAndView;
     }
@@ -35,10 +38,19 @@ public class CoursePublishController {
 
     @ResponseBody
     @PostMapping("/courseaudit/commit/{courseId}")
-    public void commitAudit(@PathVariable("courseId") Long courseId){
+    public void commitAudit(@PathVariable("courseId") Long courseId) {
         Long companyId = 1232141425L;
-        coursePublishService.commitAudit(companyId,courseId);
+        coursePublishService.commitAudit(companyId, courseId);
     }
 
 
+    @ApiOperation("课程发布")
+    @ResponseBody
+    @PostMapping("/coursepublish/{courseId}")
+    public void coursepublish(@PathVariable("courseId") Long courseId) {
+        Long companyId = 1232141425L;
+        coursePublishService.publish(companyId,courseId);
+    }
+
 }
+
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/Service/CoursePublishService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/Service/CoursePublishService.java
@@ -29,4 +29,14 @@ public interface CoursePublishService {
      */
     public void commitAudit(Long companyId,Long courseId);
 
+    /**
+     * @description 课程发布接口
+     * @param companyId 机构id
+     * @param courseId 课程id
+     * @return void
+     * @author Mr.M
+     * @date 2022/9/20 16:23
+     */
+    public void publish(Long companyId,Long courseId);
+
 }
